0d6d82e4def782f70e61f8f5f63c0e895df4bf75
Cloud User <ec2-user@ip-172-31-9-56.ec2.internal>
2017-09-06 17:36:02 +0000

    Update paypal code to prod

--- scripts/process.py
+++ scripts/process.py
@@ -11,9 +11,15 @@ from urllib import urlencode
 import urllib2
 import datetime
 
+import re
+import isodate
 
-PP_URL = "https://www.sandbox.paypal.com/cgi-bin/webscr"
-#PP_URL = "https://www.paypal.com/cgi-bin/webscr"
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+
+#PP_URL = "https://www.sandbox.paypal.com/cgi-bin/webscr"
+PP_URL = "https://www.paypal.com/cgi-bin/webscr"
 
 def confirm_paypal(f):
     
@@ -36,13 +42,41 @@ def confirm_paypal(f):
     return ret
 
 
+def verify_video(video_id):
+    # 1. Check if video in db
+    # 2. If not, check existance
+    # 3. If so, check video length
+    youtube = build("youtube", "v3", developerKey="AIzaSyAktJ5_6rO76r9rfEM6JGpdbeH2lKeo2dQ")
+
+    results = youtube.videos().list(
+        part="id,contentDetails",
+        id=video_id
+    ).execute()
+    
+    if results["items"]:
+        durationString = results["items"][0]["contentDetails"]["duration"]
+        duration = isodate.parse_duration(durationString)
+        if duration.total_seconds() <= 600: 
+          return video_id
+        else: return None
+    else: return None
+
+
+
 def write_db(f):
 
 
     prod = f["option_selection1"].value
     vidid = f["option_selection2"].value
 
+    if len(vidid) == 11: vidid = verify_video(vidid)
+    else:
+        id_regex = re.compile(r'^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*')
+        id_search = id_regex.search(vidid)
+        if id_search and id_search.group(7): vidid = verify_video(id_search.group(7)[:11])
+        else: return
     
+    if not vidid: return
 
     done = 0
 
@@ -57,7 +91,7 @@ def write_db(f):
 
     today = datetime.date.today().strftime("%Y-%m-%d")
 
-    db = MySQLdb.connect(host="localhost", user="root", passwd="Bagwell107",db="test")
+    db = MySQLdb.connect(host="localhost", user="root", passwd="Bagwell107",db="prod")
 
     query = 'INSERT INTO Votes (video_id,votes,multiplier,play_date,create_date) VALUES("%s",0,%d,0000-00-00,"%s") ON DUPLICATE KEY UPDATE multiplier=multiplier+%d;' % (db.escape_string(vidid),mod,today,mod)
 
